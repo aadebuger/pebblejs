2f2deee185eb013595e7a257b85019ac5509a766
aadebuger <admin@local.host>
2014-11-22 15:37:15 +0800

    test rect

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,38 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
+
+
+// Create the Window
+var window = new UI.Window();
+
+// Create a background Rect
+var bgRect = new UI.Rect({
+  position: new Vector2(10, 20),
+  size: new Vector2(124, 60),
+  backgroundColor: 'white'
+});
+
+// Add Rect to Window
+window.add(bgRect);
+
+// Create TimeText
+var timeText = new UI.TimeText({
+  position: new Vector2(0, 25),
+  size: new Vector2(144, 30),
+  text: "%H:%M",
+  font: 'bitham-42-bold',
+  color: 'black',
+  textAlign: 'center'
+});
+
+// Add the TimeText
+window.add(timeText);
+
+// Show the Window
+window.show();
+
+/*
 var main = new UI.Card({
   title: 'Pebble.js',
   icon: 'images/menu_icon.png',
@@ -17,6 +49,11 @@ var main = new UI.Card({
 
 
 main.show();
+
+*/
+
+
+
 /*
 main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
